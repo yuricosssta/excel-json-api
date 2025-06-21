8fe1fafb3fee922d11f5059eb06fcb36c2d6d54b
Yuri Costa <eng.yuricosta@gmail.com>
2025-06-21 17:43:39 -0300

    adaptando para vercel

--- api/data.ts
+++ api/data.ts
@@ -5,9 +5,15 @@ import dadosGenericos from '../src/models/dados.genericos';
 const MONGO_URI = process.env.MONGO_URI as string;
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  
+
   if (!mongoose.connection.readyState) {
     await mongoose.connect(MONGO_URI);
   }
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'GET') {
     try {
